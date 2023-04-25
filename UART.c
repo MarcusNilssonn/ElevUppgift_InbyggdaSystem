f2b7e2fd84882f88eda8b6343a229c41f1d49ec9
marcus.nilssonn <marcusn@live.se>
2023-04-25 13:42:09 +0200

    Testing

--- UART.c
+++ UART.c
@@ -1,21 +1,23 @@
-#include "uart.h"
+#include "UART.h"
+#include "stm32f4xx.h"
+#include <stdio.h>
 
-void USART2_Init(void){
- RCC->APB1ENR  |=  0x20000; 
- RCC->AHB1ENR   |=0x01;
+void USART2_Init(void){ //Deklarerar en funktion för att initiera UART-protokollet och dess beståndsdelar
+ RCC->APB1ENR  |=  0x20000; //Aktiverar UART2 genom att sätta bit 17 till 1.
+ RCC->AHB1ENR   |=0x01; //Aktiverar GPIOA genom att sätta bit 0 i AHB1ENR till 1.
  
-GPIOA->MODER &=~0x00F0;
-GPIOA->MODER |= 0x00A0; 
+GPIOA->MODER &=~0x00F0; //Rensar bitarna 4-7 för att förbereda pins PA2 och PA3, så att inget gammalt hänger kvar utan det sätts till 0000
+GPIOA->MODER |= 0x00A0; //Sätter bitarna 5 och 7 till 1 för att aktivera alternativ funktionalitet på pins PA2 och PA3.
 
-GPIOA->AFR[0] &= ~0xFF00;
-GPIOA->AFR[0] |= 0x7700;
+GPIOA->AFR[0] &= ~0xFF00; //Rensar bitarna 4-7 för att förbereda pins PA2 och PA3
+GPIOA->AFR[0] |= 0x7700; //Sätter bitarna 8-11 samt 12-15 till formatet 0111
 
 
-USART2->BRR  =  0x0683;
-USART2->CR1  =  0x000C; 
-USART2->CR2  =  0x000;
-USART2->CR3  =  0x000;
-USART2->CR1  |=  0x2000; 
+USART2->BRR  =  0x0683; //Sätter standard baud-rate med hjälp av hexadecimalen 0x0693 som motsvarar standard 9600
+USART2->CR1  =  0x000C; //Sätter vi tx och rx till att arbeta i 8 bitars-data. (8 bitars data, 1 stop-bit, ingen paritet.)
+USART2->CR2  =  0x000; //Nollstället CR2 för att undvika kommunikationskonflikter
+USART2->CR3  =  0x000; //Nollställer CR3
+USART2->CR1  |=  0x2000; //Omställer bit 13 (UART-aktiveringen) till 1.
 	
 	
 
--- UART.h
+++ UART.h
@@ -1,12 +1,12 @@
-ifndef  __UART_H
-#define  __UART_H
+#ifndef  __UART_H //Kolla om "__UART_H är definierat"
+#define  __UART_H //Om inte så definieras det och all kod som följer inkluderas.
 
 
-#include "stm32f4xx.h"                 
-#include <stdio.h>
+#include "stm32f4xx.h" //Inkludera bibliotek för denna microkontroller så att vi kan använda allt som finns i dokumentationen.                 
+#include <stdio.h> //Inkludera standardbibliotek.
 
-void USART2_Init(void);
-void test_setup(void);
+void USART2_Init(void); //Initiera en funktion som inte returnerar något och ej heller tar parametrar.
+void test_setup(void); //Initiera en funktion som inte returnerar något och ej heller tar parametrar.
 
 
 #endif
